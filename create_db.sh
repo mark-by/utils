702d655a4ed9478bf1f4b0ff2ec600cd3148d434
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-31 17:18:46 +0300

    refactor

--- create_db.sh
+++ create_db.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+err() {
+    echo "Usage: --db <db_name> --user <username>" 
+    echo "* Cоздать c новым юзером:     --new <password>"
+    echo "* Cоздать по sql dump'у:      --dump <dump_file>"
+    echo "* Удалить указанную базу"
+    echo "      и создать заново:       --recreate" 
+    echo "* Только удалить базу:        --drop"
+}
+
 if [ "$1" = "" ]; then
-    echo "Usage: --db <db_name> --user <username> [[if new user] --new <password>]"
+    err
     exit 1
 fi
 
@@ -17,9 +26,6 @@ password() {
     pass=$1
 }
 
-err() {
-    echo "Usage: --db <db_name> --user <username> [[if new user] --new <password>]" >&2
-}
 
 optspec=":-:"
 while getopts "$optspec" optchar; do
@@ -33,7 +39,12 @@ while getopts "$optspec" optchar; do
                     db $val;;
                 new)
                     password $val;;
-
+                recreate)
+                    recreate="true";;
+                dump)
+                    dump_file=$val;;
+                drop)
+                    dbdrop="true";;
                 *)
                     if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
                         echo "Unknown option --${OPTARG}" >&2
@@ -49,16 +60,50 @@ while getopts "$optspec" optchar; do
     esac
 done
 
-echo "db: " $db_name
-echo "user: " $db_username
-echo "password: " $pass
+drop() {
+    sudo -u postgres psql -c "drop database $db_name;"
+}
+
+if [ ! "$dbdrop" = "" ]; then
+    if [ ! "$dbdrop" = "true" ]; then
+        echo "Error: --drop должен быть последним"
+    fi
+    if [ "$db_name" = "" ]; then
+        echo "Error: Укажите, какую базу данных удалить (--db <db_name>)"
+    fi
+    drop
+    exit 0
+fi
+
+if [ "$db_name" = "" ]; then
+    err
+    exit 1
+fi
+
+
+if [ "$db_username" = "" ]; then
+    err
+    exit 1
+fi
+
+if [ ! "$recreate" = "" ]; then
+    if [ ! "$recreate" = "true" ]; then
+        echo "Error: --recreate должен быть последним"
+    fi
+    drop
+fi
 
 sudo -u postgres createdb --encoding UNICODE $db_name --username postgres
+
+if [ ! "$dump_file" = "" ]; then
+    sudo -u postgres psql $db_name < $dump_file
+fi
 
 if [ ! "$pass" = "" ]; then
     sudo -u postgres psql -c "create user $db_username with password '$pass';"
     sudo -u postgres psql -c "alter user $db_username createdb;"
 fi
+
 sudo -u postgres psql -c "grant all privileges on database $db_name to $db_username;"
 sudo -u postgres psql -d $db_name -c "grant all on all tables in schema public to $db_username;"
 sudo -u postgres psql -d $db_name -c "grant all on all sequences in schema public to $db_username;"
